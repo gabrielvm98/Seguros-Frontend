3443b5bde9586e950b3333745c0f491d41949d0f
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2024-11-18 23:01:26 -0500

    Add Filters and Sorting

--- Seguros-Frontend/src/PolicyList.jsx
+++ Seguros-Frontend/src/PolicyList.jsx
@@ -6,18 +6,45 @@ import PolicyItem from './PolicyItem'
 export default function PolicyList() {
   const [policies, setPolicies] = useState([])
 
+  const [filteredPolicies, setFilteredPolicies] = useState([])
+  const [filter, setFilter] = useState({ tipo: ''})
+  const [sortBy, setSortBy] = useState('')
+
   useEffect(() => {
     fetchPolicies()
   }, [])
+
+  useEffect(() => {
+    applyFiltersAndSorting()
+  }, [policies, filter, sortBy])
 
   const fetchPolicies = async () => {
     try {
       const response = await axios.get('http://localhost:8080/segurosAPI/v1/polizas')
       setPolicies(response.data.data)
-      console.log(policies)
+      
     } catch (error) {
       console.error('Failed to fetch policies:', error)
     }
+  }
+
+  const applyFiltersAndSorting = () => {
+    let updatedPolicies = [...policies]
+        
+
+    if (filter.tipo) {
+      updatedPolicies = updatedPolicies.filter(policy => policy.tipo === filter.tipo)
+    }  
+
+    
+    
+    if (sortBy === 'monto') {
+      updatedPolicies.sort((a, b) => a.monto - b.monto)
+    } else if (sortBy === 'fechaInicio') {
+      updatedPolicies.sort((a, b) => new Date(a.fechaInicio) - new Date(b.fechaInicio))
+    }
+
+    setFilteredPolicies(updatedPolicies)
   }
 
   const handleDelete = async (id) => {
@@ -32,8 +59,24 @@ export default function PolicyList() {
   return (
     <div className="card">
       <h2 className="card-title">Listado de Polizas de Seguro</h2>
+
+      <div className="filters">
+        <select onChange={(e) => setFilter({ ...filter, tipo: e.target.value })}>
+          <option value="">Filtrar por Tipo</option>
+          <option value="AUTO">Auto</option>
+          <option value="INMUEBLE">Inmueble</option>
+          <option value="CELULAR">Celular</option>
+        </select>
+        
+        <select onChange={(e) => setSortBy(e.target.value)}>
+          <option value="">Ordenar por</option>
+          <option value="monto">Monto</option>
+          <option value="fechaInicio">Fecha de Inicio</option>
+        </select>
+      </div>
+
       <div className="policy-list">        
-        {policies.map((policy) => (
+        {filteredPolicies.map((policy) => (
           <PolicyItem key={policy.id} policy={policy} onDelete={handleDelete} />
         ))}
       </div>
